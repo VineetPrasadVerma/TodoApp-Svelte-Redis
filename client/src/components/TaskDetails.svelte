2988376bdbf07956d6ef565772495d6e8b4128f1
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-05-07 23:24:38 +0530

    Edit and delete task functionality implemented

--- client/src/components/TaskDetails.svelte
+++ client/src/components/TaskDetails.svelte
@@ -1,28 +1,119 @@
 <script>
-    import Icon from 'fa-svelte'
-    import { faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons/'
-    // import { faPencilSquare } from '@fortawesome/free-solid-svg-icons/faPencilSquare'
+  import TaskStore from '../stores/taskStore.js'
+  import Icon from 'fa-svelte'
+  import { faTrash, faPencilAlt } from '@fortawesome/free-solid-svg-icons/'
 
-    export let task
+  export let task
+  let isEditing = false
+  let updatedTaskName = task.taskName
 
-    function handleClick(id) {
-        console.log(id, 'Trash Clicked') 
+  const baseURL = 'http://localhost:3000/tasks'
+
+  const fetchAPI = async (reqObj) => {
+    try {
+      const res = await window.fetch(reqObj.url, reqObj.init)
+      const data = await res.json()
+
+      if (res.status === 200 || res.status === 201) {
+        return data
+      } else if (res.status === 500) {
+        //  showError(res.status, data)
+        return null
+      } else if (res.status === 404) {
+        //  showError(res.status, data)
+        return null
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const readTasksDB = async () => {
+    const reqObj = {
+      url: baseURL + '/',
+      init: {
+        method: 'GET'
+      }
     }
 
+    let tasks = await fetchAPI(reqObj)
+    if (tasks != null && tasks.taskCount !== 0) {
+      return tasks
+    } else {
+      tasks = []
+      return null
+    }
+  }
+
+  async function deleteTask(id) {
+    const reqObj = {
+      url: baseURL + `/${id}`,
+      init: {
+        method: 'DELETE'
+      }
+    }
+
+    const result = await fetchAPI(reqObj)
+
+    if(result){
+      const tasks = await readTasksDB()
+      if(tasks) $TaskStore = tasks
+      else {$TaskStore = []}
+    }
+
+    else{
+        //Show Error Page
+    }
+
+  }
+
+  async function updateTask(id) {
+    if(event.keyCode === 13){
+
+      if(taskName === ''){
+        event.target.placeholder = 'Can\'t add empty Task'
+        return
+      }
+
+      const reqObj = {
+        url: baseURL + `/${id}`,
+        init: {
+          method: 'PUT',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify({ taskName: updatedTaskName })
+        }
+      }
+
+      const result = await fetchAPI(reqObj)
+      if(result){
+        const tasks = await readTasksDB()
+        if(tasks) $TaskStore = tasks
+      }
+
+      isEditing = false
+    }
+  }
+
+  function showEditInputField() {
+      isEditing = true
+  }
 </script>
 
 <div id={task.taskId}>
-    <span id='list-item'>{task.taskName}</span>
-    <span on:click={() => handleClick(task.taskId)}><Icon icon={faTrash}  ></Icon></span>
-    <Icon icon={faPencilAlt}></Icon>
+    <span id='taskName'>
+        {#if isEditing}
+          <input type="text" bind:value={updatedTaskName} autofocus on:keyup={() => updateTask(task.taskId)}>
+        {:else}
+            {task.taskName}
+            <span id='deleteIcon' on:click={() => deleteTask(task.taskId)}><Icon icon={faTrash}/></span>
+            <span id='editIcon' on:click={() => showEditInputField()}><Icon icon={faPencilAlt}/></span>
+        {/if}
+    </span>
+    
 </div>
 
 <style>
-    /* #list-item{
-        color:red;
-    } */
-
-    /* Icon{
-        color: red;
-    } */
+  /* #taskName{
+      float: left;
+  } */
 </style>
